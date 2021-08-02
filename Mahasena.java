5d40403dc630398ea2688898344cf3044082cc46
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-08-02 22:51:26 +0600

    solved

---
+++ Mahasena.java
@@ -0,0 +1,29 @@
+
+/*
+    Solved by Rutton Chandra Sarker
+    North South University
+*/
+import java.util.*;
+
+public class Mahasena {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        int even = 0, odd = 0;
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            if (a[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        if (even > odd) {
+            System.out.println("READY FOR BATTLE");
+        } else {
+            System.out.println("NOT READY");
+        }
+        sc.close();
+    }
+}
