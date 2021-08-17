ed85e886b546c2cc5b117c6559c4a3e5f149a2bc
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-08-17 23:43:01 +0600

    solved

---
+++ Codechef Starters 9 (Div 3)/BlackCellsChessboard.java
@@ -0,0 +1,15 @@
+
+/*
+    CodeChef Starters 9 Division 3
+    solved by Rutton Chandra Sarker
+*/
+import java.util.*;
+
+public class BlackCellsChessboard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n * n / 2);
+        sc.close();
+    }
+}
