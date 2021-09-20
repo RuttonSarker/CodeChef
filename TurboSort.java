2ec865d600a904700af37ad94525149ab3d7f7a3
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-09-20 22:25:18 +0600

    pending

---
+++ TurboSort.java
@@ -0,0 +1,18 @@
+
+/*
+    Solved by Rutton Chandra Sarker
+    North South University
+*/
+import java.util.*;
+
+public class TurboSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int a[] = new int[t];
+
+        for (int i = 0; i < t; i++) {
+            a[i] = sc.nextInt();
+        }
+    }
+}
