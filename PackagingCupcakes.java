5fad50879e71e2ea0de6355df259aa0b852e9507
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-09-20 22:26:42 +0600

    pending

---
+++ PackagingCupcakes.java
@@ -0,0 +1,21 @@
+
+/*
+    Solved by Rutton Chandra Sarker
+    North South University
+*/
+import java.util.*;
+
+public class PackagingCupcakes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t != 0) {
+            int n = sc.nextInt();
+            if (n % 2 == 0) {
+                System.out.println(n);
+            } else {
+                System.out.println();
+            }
+        }
+    }
+}
